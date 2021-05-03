a2ce485871396f03b92e7fdd71a223f2539b71cf
Simon <a20simst@student.his.se>
2021-05-03 09:00:07 +0200

    Added about us activity

--- app/src/main/java/com/example/project_examination/MainActivity.java
+++ app/src/main/java/com/example/project_examination/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.project_examination;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button omOss = findViewById(R.id.om_oss);
+
+        omOss.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
