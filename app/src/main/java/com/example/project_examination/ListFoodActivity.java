aab084e24b42679751d4caaf56d9d2ca25d83f53
Simon <a20simst@student.his.se>
2021-05-07 11:29:03 +0200

    Added Json task and New JsonTask execute. Added AsyncTask
    Added a food array and new gson in onPostExecute. Added Clear for foodlist and a forloop to add food. to foodList.

--- app/src/main/java/com/example/project_examination/ListFoodActivity.java
+++ app/src/main/java/com/example/project_examination/ListFoodActivity.java
@@ -2,12 +2,24 @@ package com.example.project_examination;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class ListFoodActivity extends AppCompatActivity {
@@ -21,12 +33,77 @@ public class ListFoodActivity extends AppCompatActivity {
         ListView listView;
         listView = findViewById(R.id.list_view);
         adapter = new ArrayAdapter<Food>(ListFoodActivity.this, R.layout.list_food_textview,foodList);
-
+        listView.setAdapter(adapter);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+
+        new JsonTask().execute("https://wwwlab.iit.his.se/brom/kurser/mobilprog/dbservice/admin/getdataasjson.php?type=a20simst");
     }
+    @SuppressLint("StaticFieldLeak")
+    private class JsonTask extends AsyncTask<String, String, String> {
+
+        private HttpURLConnection connection = null;
+        private BufferedReader reader = null;
+
+        protected String doInBackground(String... params) {
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuilder builder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null && !isCancelled()) {
+                    builder.append(line).append("\n");
+                }
+                return builder.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String json) {
+            Log.d("TAG", json);
+            Gson gson = new Gson();
+
+            Food[] mountains;
+            mountains = gson.fromJson(json,Food[].class);
+
+
+            foodList.clear();
+            for (int i = 0; i < mountains.length; ++i) {
+
+                foodList.add(mountains[i]);
+            }
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+
+
+
+
+
 }
