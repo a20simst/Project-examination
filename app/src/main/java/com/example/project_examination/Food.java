cc7a09c42e7094c6102868004a4e59f77b1c46e7
Simon <a20simst@student.his.se>
2021-05-07 11:33:58 +0200

    Added getters to food class.

--- app/src/main/java/com/example/project_examination/Food.java
+++ app/src/main/java/com/example/project_examination/Food.java
@@ -17,4 +17,36 @@ public class Food {
     public String toString() {
         return name ;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public Auxdata getAuxdata() {
+        return auxdata;
+    }
 }
