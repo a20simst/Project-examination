733cc066d2205a5a0d9a43d4617a7b874f7f4701
Simon <a20simst@student.his.se>
2021-05-07 11:12:26 +0200

    Added arraylist and adapter. Created Listview variable. Gave the listView the id from listview. Created arraylist in adapter including layout from textview and arraylist.

--- app/src/main/java/com/example/project_examination/ListFoodActivity.java
+++ app/src/main/java/com/example/project_examination/ListFoodActivity.java
@@ -4,15 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ListFoodActivity extends AppCompatActivity {
-
+    private ArrayList<Food> foodList = new ArrayList<>();
+    private ArrayAdapter<Food> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_food);
         Button back = findViewById(R.id.back);
+        ListView listView;
+        listView = findViewById(R.id.list_view);
+        adapter = new ArrayAdapter<Food>(ListFoodActivity.this, R.layout.list_food_textview,foodList);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
