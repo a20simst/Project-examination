cd0ab4879d874f30157c5e1ee052e0630407112e
Simon <a20simst@student.his.se>
2021-05-07 11:00:59 +0200

    Added classes for my json data.

---
+++ app/src/main/java/com/example/project_examination/Auxdata.java
@@ -0,0 +1,7 @@
+package com.example.project_examination;
+
+public class Auxdata {
+
+    private String wiki;
+    private String img;
+}
---
+++ app/src/main/java/com/example/project_examination/Food.java
@@ -0,0 +1,15 @@
+package com.example.project_examination;
+
+public class Food {
+
+
+    private String ID;
+    private String name;
+    private String type;
+    private String company;
+    private String location;
+    private String category;
+    private int size;
+    private int cost;
+    private Auxdata auxdata;
+}
