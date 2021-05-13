b90dbbb8124ab8da52d16fd3ebd06cfe64c65396
Simon <a20simst@student.his.se>
2021-05-13 14:25:53 +0200

    Added a WebView to ListFoodActivity.java and to activity_list_food.xml

--- app/src/main/java/com/example/project_examination/ListFoodActivity.java
+++ app/src/main/java/com/example/project_examination/ListFoodActivity.java
@@ -7,6 +7,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -27,6 +28,7 @@ import java.util.ArrayList;
 public class ListFoodActivity extends AppCompatActivity {
     private ArrayList<Food> foodList = new ArrayList<>();
     private ArrayAdapter<Food> adapter;
+    private WebView myWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
